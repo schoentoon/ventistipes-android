00d59ec61d264d65432a4e0800927b4ef85f61e5
Toon Schoenmakers <nighteyes1993@gmail.com>
2013-10-03 00:34:23 +0200

    Upgraded to the new way of handling gcm
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- src/com/schoentoon/ventistipes/GCMIntentService.java
+++ src/com/schoentoon/ventistipes/GCMIntentService.java
@@ -1,16 +1,22 @@
 package com.schoentoon.ventistipes;
 
+import android.app.IntentService;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import com.google.android.gcm.GCMBaseIntentService;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
 import java.util.Iterator;
 import java.util.Random;
 
-public class GCMIntentService extends GCMBaseIntentService {
+public class GCMIntentService extends IntentService {
+  public GCMIntentService() {
+    super(GCMIntentService.class.getName());
+  }
+
   protected void onMessage(Context context, Intent intent) {
     final Bundle bundle = intent.getExtras();
     for (Iterator<String> iter = bundle.keySet().iterator();iter.hasNext();) {
@@ -30,23 +36,27 @@ public class GCMIntentService extends GCMBaseIntentService {
     nm.notify(new Random().nextInt(), builder.build());
   }
 
-  protected void onError(Context context, String s) {
-    broadcast("error", s);
-  }
-
-  protected void onRegistered(Context context, String s) {
-    broadcast("registered", s);
-  }
-
-  protected void onUnregistered(Context context, String s) {
-    broadcast("unregistered", s);
-  }
-
-  private void broadcast(String type, String s) {
-    Intent intent = new Intent(getClass().getName());
-    intent.putExtra("type", type);
-    intent.putExtra("msg", s);
-    sendBroadcast(intent);
-    Log.d(type, s);
+  protected void onHandleIntent(Intent intent) {
+    Bundle extras = intent.getExtras();
+    GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+    String messageType = gcm.getMessageType(intent);
+    if (!extras.isEmpty() && GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+      for (Iterator<String> iter = extras.keySet().iterator();iter.hasNext();) {
+        String key = iter.next();
+        Log.d(extras.getClass().getSimpleName(), "Key: " + key + ", data: " + extras.get(key).toString());
+      }
+      NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+      builder.setContentTitle(extras.getString("subject","No subject"));
+      builder.setContentText(extras.getString("data", "No data"));
+      builder.setSubText(extras.getString("sender", "No sender"));
+      builder.setStyle(new NotificationCompat.BigTextStyle(builder)
+        .bigText(extras.getString("data", "No data"))
+        .setBigContentTitle(extras.getString("subject", "No subject"))
+        .setSummaryText(extras.getString("sender", "No sender")));
+      builder.setSmallIcon(android.R.drawable.stat_notify_error);
+      NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+      nm.notify(new Random().nextInt(), builder.build());
+    }
+    GCMBroadcastReceiver.completeWakefulIntent(intent);
   }
 }
--- src/com/schoentoon/ventistipes/MainActivity.java
+++ src/com/schoentoon/ventistipes/MainActivity.java
@@ -1,65 +1,136 @@
 package com.schoentoon.ventistipes;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.*;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import com.google.android.gcm.GCMRegistrar;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    checkPlayServices();
+    gcm = GoogleCloudMessaging.getInstance(this);
     setContentView(R.layout.main);
     findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
       public void onClick(View view) {
         EditText proj_id = (EditText) findViewById(R.id.proj_id);
-        GCMRegistrar.register(MainActivity.this, proj_id.getText().toString());
+        registerInBackground(MainActivity.this, proj_id.getText().toString());
       }
     });
-    findViewById(R.id.unregister).setEnabled(GCMRegistrar.isRegistered(this));
+    findViewById(R.id.unregister).setEnabled(getRegistrationId(this) != null);
     findViewById(R.id.unregister).setOnClickListener(new View.OnClickListener() {
       public void onClick(View view) {
-        GCMRegistrar.unregister(MainActivity.this);
+        unregisterInBackground(MainActivity.this);
       }
     });
-    findViewById(R.id.share).setEnabled(GCMRegistrar.isRegistered(this));
+    findViewById(R.id.share).setEnabled(getRegistrationId(this) != null);
     findViewById(R.id.share).setOnClickListener(new View.OnClickListener() {
       public void onClick(View view) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, GCMRegistrar.getRegistrationId(MainActivity.this));
+        intent.putExtra(Intent.EXTRA_TEXT, getRegistrationId(MainActivity.this));
         startActivity(intent);
       }
     });
-    if (GCMRegistrar.isRegistered(this))
-      ((TextView) findViewById(R.id.push_id)).setText(GCMRegistrar.getRegistrationId(this));
-    registerReceiver(broadcastReceiver, new IntentFilter(GCMIntentService.class.getName()));
+    if (getRegistrationId(this) != null)
+      ((TextView) findViewById(R.id.push_id)).setText(getRegistrationId(this));
   }
 
-  protected void onDestroy() {
-    super.onDestroy();
-    GCMRegistrar.onDestroy(this);
-    unregisterReceiver(broadcastReceiver);
-  }
+  private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
-  private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-    public void onReceive(Context context, Intent intent) {
-      final String type = intent.getStringExtra("type");
-      if ("registered".equals(type)) {
-        findViewById(R.id.unregister).setEnabled(true);
-        findViewById(R.id.share).setEnabled(true);
-        ((TextView) findViewById(R.id.push_id)).setText(intent.getStringExtra("msg"));
-      } else if ("unregistered".equals(type)) {
-        findViewById(R.id.unregister).setEnabled(false);
-        findViewById(R.id.share).setEnabled(false);
-        ((TextView) findViewById(R.id.push_id)).setText("");
-      } else if ("error".equals(type))
-        ((TextView) findViewById(R.id.push_id)).setText("Error: " + intent.getStringExtra("msg"));
+  private boolean checkPlayServices() {
+    int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+    if (resultCode != ConnectionResult.SUCCESS) {
+      if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+        GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+          PLAY_SERVICES_RESOLUTION_REQUEST).show();
+      } else {
+        Log.i(getClass().getName(), "This device is not supported.");
+        finish();
+      }
+      return false;
     }
-  };
+    return true;
+  }
+
+  private void unregisterInBackground(final Context context) {
+    new AsyncTask<Void, Void, String>() {
+      protected String doInBackground(Void... voids) {
+        try {
+          gcm.unregister();
+        } catch (IOException e) {
+          return "Error: " + e.getMessage();
+        }
+        return "Successfully unregistered device";
+      }
+
+      protected void onPostExecute(String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        if (getRegistrationId(MainActivity.this) != null) {
+          ((TextView) findViewById(R.id.push_id)).setText(getRegistrationId(MainActivity.this));
+          findViewById(R.id.share).setEnabled(getRegistrationId(MainActivity.this) != null);
+        } else
+          ((TextView) findViewById(R.id.push_id)).setText("");
+      }
+    }.execute();
+  }
+
+  private void registerInBackground(final Context context, String sender_id) {
+    new AsyncTask<String, Void, String>() {
+      protected String doInBackground(String... params) {
+        String msg = "";
+        try {
+          String regid = gcm.register(params[0]);
+          msg = "Device registered, registration ID=" + regid;
+          storeRegistrationId(context, regid);
+        } catch (IOException ex) {
+          msg = "Error :" + ex.getMessage();
+        }
+        return msg;
+      }
+
+      protected void onPostExecute(String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        if (getRegistrationId(MainActivity.this) != null) {
+          ((TextView) findViewById(R.id.push_id)).setText(getRegistrationId(MainActivity.this));
+          findViewById(R.id.share).setEnabled(getRegistrationId(MainActivity.this) != null);
+        } else
+          ((TextView) findViewById(R.id.push_id)).setText("");
+      }
+    }.execute(sender_id);
+  }
+
+  private static final String PROPERTY_REG_ID = "register_id";
+
+  private void storeRegistrationId(Context context, String regId) {
+    final SharedPreferences prefs = getGCMPreferences(context);
+    SharedPreferences.Editor editor = prefs.edit();
+    editor.putString(PROPERTY_REG_ID, regId);
+    editor.commit();
+  }
+
+  private String getRegistrationId(Context context) {
+    final SharedPreferences prefs = getGCMPreferences(context);
+    String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+    if (registrationId.isEmpty()) {
+      Log.i(getClass().getName(), "Registration not found.");
+      return null;
+    }
+    return registrationId;
+  }
+
+  private SharedPreferences getGCMPreferences(Context context) {
+    return getSharedPreferences(getClass().getSimpleName(), Context.MODE_PRIVATE);
+  }
+
+  private GoogleCloudMessaging gcm;
 }
